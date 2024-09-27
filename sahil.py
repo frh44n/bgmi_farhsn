43d8f949eb83683f3392947b9bc52b5eac15f0b8
frh44n <107492548+frh44n@users.noreply.github.com>
2024-09-27 16:45:27 +0530

    Update sahil.py

--- sahil.py
+++ sahil.py
@@ -305,5 +305,5 @@ def remove_webhook():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     # Use a default port if not provided in the environment
-    port = 5000
+    port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port)
